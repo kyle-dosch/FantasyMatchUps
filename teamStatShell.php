d18071574380f7f51ceec458aca013f83b073a29
Kyle Dosch <kyle.dosch630@gmail.com>
2015-01-13 16:05:37 -0500

    Update teamStatShell.php

--- teamStatShell.php
+++ teamStatShell.php
@@ -1,5 +1,18 @@
+<?php
 
 class teamStatShell(){
+  
+  public $teamRank = 0;
+  public $teamName = '';
+  public $ReboundsAllowed = 0;
+  public $playerAsts = 0;
+  public $playerSteals = 0;
+  public $playerBlocks = 0;
+  public $playerTurnOvers = 0;
+  public $playerPoints = 0;
+  public $PlayerValue = 0;
 
 
 }
+
+?>
