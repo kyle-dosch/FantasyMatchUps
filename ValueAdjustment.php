73128877f583269b36be93261c3e1883b62c4d17
Kyle Dosch <kyle.dosch630@gmail.com>
2015-01-15 00:36:25 -0500

    Update ValueAdjustment.php

--- ValueAdjustment.php
+++ ValueAdjustment.php
@@ -3,7 +3,22 @@
 
 class ValueAdjustment(){
 
-
+  protected $players = array();
+  protected $teams = array();
+  protected $playerVals = array();
+  protected $SQLplayers = '';
+  protected $SQLteam = '';
+  protected $OffTeam = '';
+  protected $DefTeam = '';
+  
+  public function fillArrays(){
+    
+    $db = new StatsDataBase()
+    
+    
+    
+  }
+  
 
 
 
