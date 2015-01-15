6769ca5da2b1cae1f5b725b8a3ee1bc2f3d917af
Kyle Dosch <kyle.dosch630@gmail.com>
2015-01-15 01:25:49 -0500

    Update ValueAdjustment.php

--- ValueAdjustment.php
+++ ValueAdjustment.php
@@ -10,23 +10,66 @@ class ValueAdjustment(){
   protected $SQLteam = '';
   protected $OffTeam = '';
   protected $DefTeam = '';
+  protected $playerValsTbl = array();
   
-  public function fillArrays(){
+  public function fillArrays($db,$Oteam,$Dteam){
     
-    $db = new StatsDataBase()
+    $this->SQLplayers = "SELECT * FROM Players WHERE team = ".$this->OffTeam;
+    $this->SQLteam = "SELECT * FROM Teams WHERE name = ".$this->DefTeam;
+    
+    $db->makeDBConn;
+    
+    $stats = $db->conn->query($this->SQLteam);
+    if ($stats->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+        //insert team values into Teamarray
+      }
+    }
     
     
-    
-  }
+    $stats = $db->conn->query($this->SQLplayers);
+    if ($stats->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+        //insert values into players array
+        $this->calcValues();
+        $this->playerValsTbl->push($this->playerVals);
+      }
+    }
+   
+   $this->intoHTMLTable();
+   
+    $db->DBshutDown;
   
-
-
-
-
-
-
-
-
+  protected function calcValues(){
+    
+    //Calc values for one player and insert into player values array 
+    /**
+     * ------WEIGHTS SCALE-------
+     * --Defensive Rank per cat-- * --Players avg for cat-- = weight
+     * 
+     *      (1)-(-3.2)           *        pAvg        =      Players new value = weight +- pAvg
+     *      (2)-(-3.0)
+     *      (3)-(-2.8)
+     *       .
+     *       .
+     *      (15)-(-0.2)
+     *      (16)-(+0.2)
+     *       .
+     *       .
+     *      (30)-(+2.8)
+     *      (31)-(+3.0)
+     *      (32)-(+3.2)
+    */
+    
+    
+  }   
+    
+    
+  protected function intoHTMLTable(){
+    
+    //publish results of players vals table
+    
+  }  
 }
 
 ?>
